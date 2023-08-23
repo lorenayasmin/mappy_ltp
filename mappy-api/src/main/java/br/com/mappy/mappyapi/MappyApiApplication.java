5369480d82714aca1609a0dbcd1c947937adc4b7
lorenayasmin <141173962+lorenayasmin@users.noreply.github.com>
2023-08-23 14:34:29 -0300

    mappy-api
    Subindo primeiro projeto

---
+++ mappy-api/src/main/java/br/com/mappy/mappyapi/MappyApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.mappy.mappyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MappyApiApplication.class, args);
+	}
+
+}
---
+++ mappy-api/src/test/java/br/com/mappy/mappyapi/MappyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mappy.mappyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
