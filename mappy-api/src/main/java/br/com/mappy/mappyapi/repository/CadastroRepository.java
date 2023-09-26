e902ff2cc25ce57aa31a98ced74e9ebd956e8dd9
unknown <victoria.v@aluno.ifsp.edu.br>
2023-09-26 15:07:05 -0300

    Subindo a parte de criação dos arquivos repository e de um projeto no mysql

---
+++ mappy-api/src/main/java/br/com/mappy/mappyapi/repository/CadastroRepository.java
@@ -0,0 +1,4 @@
+package br.com.mappy.mappyapi.repository;
+
+public class CadastroRepository <Cadastro, Integer>{
+}
---
+++ mappy-api/src/main/java/br/com/mappy/mappyapi/repository/ForumRepository.java
@@ -0,0 +1,5 @@
+package br.com.mappy.mappyapi.repository;
+
+public class ForumRepository {
+    
+}
