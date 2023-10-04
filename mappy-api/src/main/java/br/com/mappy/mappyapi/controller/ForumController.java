e8a5435375868088dca63a4b20fb215a84eec6ff
unknown <nycole.dias@aluno.ifsp.edu.br>
2023-10-04 14:15:34 -0300

    subindo o POST do ForumController

--- mappy-api/src/main/java/br/com/mappy/mappyapi/controller/ForumController.java
+++ mappy-api/src/main/java/br/com/mappy/mappyapi/controller/ForumController.java
@@ -2,6 +2,7 @@ package br.com.mappy.mappyapi.controller;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,10 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.mappy.mappyapi.model.Forum;
 import br.com.mappy.mappyapi.model.ForumDAO;
+import br.com.mappy.mappyapi.repository.CadastroRepository;
 import br.com.mappy.mappyapi.repository.ForumRepository;
 
 @RestController
 public class ForumController {
+   @Autowired
+    ForumRepository forumRepository;
 
   @PutMapping("/api/forum/postagens")
   ArrayList forum() {
